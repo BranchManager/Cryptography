683790891fec8162f6a36e5d28aa962216e10668
noah Branch <nbranch@vols.utk.edu>
2019-09-25 18:25:49 -0400

    if you look at the key_bits function it does cbc mode

--- cbc-enc.py
+++ cbc-enc.py
@@ -1,4 +1,6 @@
 import argparse
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 def parse_args():
 	parser = argparse.ArgumentParser()
@@ -14,14 +16,32 @@ def parse_args():
 	if args.key:
 		keyf = args.key
 	return inputf, outputf, keyf
+def key_bits(keyinn):
+	#print(keyinn)
+	#AES_KEY = int(keyinn, 16).to_bytes(len(key) // 2, 'little')
+	r = bytearray.fromhex(keyinn)
+	print(r)
+	iv = b'1234512345kilifq'
 
+	data = b'hello to all of you young niggas I hope all is well'
+	cipher = AES.new(r, AES.MODE_CBC,iv)
+	ciphertext = cipher.encrypt(pad(data,16))
+	print(ciphertext)
 
+	cipher2 = AES.new(r,AES.MODE_CBC,iv)
+	print(type(ciphertext))
+	plaintext = cipher2.decrypt(ciphertext)
+	print("Plaintext \n")
+	print(plaintext)
+	#print(AES_KEY.bit_length())
 if __name__=="__main__":
 
 	infile,outfile,key = parse_args()
 	
-	f = open(infile,'r')
+	f = open(infile,'rb')
 	inn = f.read()
 
 	fkey = open(key,'r')
 	keyinn = fkey.read()
+	
+	key_bits(keyinn)
