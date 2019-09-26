79534d7bb4ce45c244a7a02af1af624da010881b
noah Branch <nbranch@vols.utk.edu>
2019-09-26 12:51:48 -0400

    you get it now mike

--- cbc-mac.py
+++ cbc-mac.py
@@ -1,8 +1,9 @@
 import argparse
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
+#from Crypto.Util.Padding import pad
 from Crypto.Util.strxor import strxor
 from sys import getsizeof
+from Crypto.Random import get_random_bytes
 BLOCK_SIZE = 16
 
 def parse_args_mac():
@@ -18,7 +19,7 @@ def parse_args_mac():
 		messagef = args.message
 	if args.key:
 		keyf = args.key
-	return messagef,tagf, keyf
+	return messagef,tagf,keyf
 #paddit takes a binary string so of format (b'Iam the string to pad)
 def padit(data_to_pad):
 	#print("TRY PAD")
@@ -56,13 +57,39 @@ def divide_into_blocks(message):
 	# print('for loop')
 	# for i in INPUT:
 	# 	print(i)
-#def encrypt(key,block)
+def encrypt(key,block):
+	Fk= AES.new(key,AES.MODE_ECB)
+	ciphertext = Fk.encrypt(block)
+	return ciphertext
 
-def cbcmac(key,message_blocks):
-	cipher = b'0'
-	for i in range(message_blocks):
-		xored_string = strxor(cipher,message_blocks[i])
-		cipher = encrypt(key,xored_string)
+def cbcmac(key,message_blocks,cbcmac_flag):
+	print(len(key))
+	#exit()
+	cipherblocks=[]
+	if cbcmac_flag == 1:
+		cipherblock = b'\x00'*16
+	else:
+		cipherblock = get_random_bytes(16)
+		iv = cipherblock
+	# for i in range(message_blocks):
+	print(len(message_blocks[0]))
+	print(len(cipherblock))
+	print(len(message_blocks))
+	for i in range(len(message_blocks)):
+		print("xor length")
+		print(len(cipherblock))
+		print(message_blocks[i])
+		print(len(message_blocks[i]))
+		xored_string = strxor(cipherblock,message_blocks[i])
+		print("xor length AFTER xor")
+		print(len(cipherblock))
+		print(len(message_blocks[i]))
+		cipherblock = encrypt(key,xored_string)
+		print("LENGTH")
+		print(len(cipherblock))
+		cipherblocks.append(cipherblock)
+	return cipherblocks[-1],cipherblocks,iv
+	
 
 #def tag_it(key):
 print("hello here")
@@ -72,8 +99,8 @@ if __name__=="__main__":
 	omessagf=open(messagef,'rb')
 	message = omessagf.read()
 
-	okeyf = open(keyf, 'rb')
-	key = omessagf.read()
+	okeyf = open(keyf)
+	key = okeyf.read()
 
 	padded_message = padit(message)
 	print(len(message))
@@ -82,7 +109,13 @@ if __name__=="__main__":
 
 	blocks=divide_into_blocks(padded_message)
 
-	tag=cbcmac(key,blocks)
+	
+	
+	print(key)
+	bytekey = bytes.fromhex(key)
+	print(len(bytekey))
+	#exit()
+	the_tag,cipherblocks,iv = cbcmac(bytekey,blocks,1)
 	
 
 	
