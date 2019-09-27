3c279191d8560932a5541944232887da6f4c864a
Mkang15 <mkang15@vols.utk.edu>
2019-09-27 03:40:55 -0400

    some stuff

--- ctr-enc.py
+++ ctr-enc.py
@@ -1,35 +1,49 @@
 from cbc_modes import *
 
 def CTR(blocks,outfile,key):	
-	i = 1
+	i = 0
+	x = 1
 	cText = []
 	
 	cipher = AES.new(key,AES.MODE_ECB)
-	IV = cipher.encrypt(key)
-	
-	cNext = strxor(cipher.encrypt(IV),blocks[0])
-	cText.append(cNext)
+	reth = key
+	IV = cipher.encrypt(reth[16:])
 
-	while(i < len(blocks)):
-		IV = IV+1
-		cNext = strxor(cipher.encrypt(IV),blocks[i])
+	print(type(IV[0]))
+	while(i < len(blocks)-1):
+
+		cNext = strxor(cipher.encrypt(IV),blocks[i])			 
+
+		if IV[len(IV)-1] < 16:
+			bytes = IV.replace(IV[len(IV)-x],bytes([IV[len(IV)-x] + 1]))
+		else:
+			x = x+1
+			bytes = IV.replace(IV[len(IV)-x],bytes([IV[len(IV)-x] + 1]))
+		
 		i = i+1
+		
 		cText.append(cNext)
 	
-	file = open(outfile,"w")
-	file.write(cText)
+	file = open(outfile,"wb")
+	for i in cText:
+		file.write(i)
 
 
 if __name__=="__main__":
 
-	infile,outfile,key = argparse()
+	key,infile,outfile = parse_args()
 	
 	f = open(infile,'rb')
 	inn = f.read()
 
 	fkey = open(key,'r')
+	
 	keyinn = fkey.read()
+	
+	keyinn = keyinn.rstrip('\t\r\n\0')
+
+	bytekey = bytes.fromhex(keyinn)
 
 	blockArr = divide_into_blocks(inn)
 	
-	CTR(blockArr,outfile,key)
+	CTR(blockArr,outfile,bytekey)
