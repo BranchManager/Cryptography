f239bca464e41f1a0a250c01ff58cd8e228b5bd8
Mkang15 <mkang15@vols.utk.edu>
2019-11-11 12:37:39 -0500

    RSA ENC and RSA DEC

--- mRSA_dec.py
+++ mRSA_dec.py
@@ -3,14 +3,8 @@ import argparse
 
 import sys
 sys.path.insert(1,"../PyPl/PyPl")
+import random
 
-from Crypto.Cipher import AES
-from multiprocessing import Pool
-from Crypto.Util.strxor import strxor
-from sys import getsizeof
-from Crypto.Random import get_random_bytes
-from Crypto.Util import number
-from Crypto.Util.number import long_to_bytes
 
 def parse_args():
 	parser = argparse.ArgumentParser()
@@ -27,57 +21,41 @@ def parse_args():
 		outfile = args.output
 	return key,infile,outfile
 
-def RSA_ENC(r,mesBitLen,mes,e,N):
-	
-	if r-24 < 0:
-		print("n is too small")
-		return -1
+def RSA_Dec(c,d,N):
+	m = c**d%N
+	return m
 
-	if mesBitLen > r-24:
-		print("Error Message is too Large")
-		return -1
-	
-	rpad = random.getrandbits(r)
-	rpad = rpad.to_bytes()
-	st = b'0x00'
-	ed = b'0x02'
-	mes = mes.to_bytes()
-	paddedm = st+ed+rpad+st+mes
-
-	m = paddedm.to_int()
-	c = m**e%N
-	return c
-	
 
 if __name__ =="__main__":
     key,infile,outfile = parse_args()
 
 	
     f = open(infile,'r')
-    m = f.read()
-	m = int(m)
+    c = f.read()
+	c = int(c)
 
 
     fkey = open(key,'r')
     key = fkey.readlines()
 
 	f.close()
-	k = int(key[0])
+
+	LenN = int(key[0])
 	r = k//2
 
-	e = int(key[2])
+	d = int(key[2])
 	N = int(key[1])
 
-	D = m.bit_length()
+	Clen = m.bit_length()
 	
 	if D%2 == 1:
 		D+=1
 	
-	c = RSA_ENC(r,D,m,e,N)
+	m = RSA_Dec(c,d,N)
+
 	f.open(outfile,'w')
-	f.write(c)
+	f.write(m)
 	f.close()
 
-	print (c)
     print(type(key))
     print(key)
