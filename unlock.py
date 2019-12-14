6932e4177e5ba38a110895aae2acf04397d96352
noah Branch <nbranch@vols.utk.edu>
2019-12-13 23:56:22 -0500

    updae

--- unlock.py
+++ unlock.py
@@ -6,6 +6,7 @@ from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import utils
 from cryptography.hazmat.primitives.asymmetric import rsa,ec
 
 def parse_args():
@@ -27,7 +28,32 @@ def parse_args():
 
     return Dir,pubk,prik,sub
 
+def verify(signature, Public_ec_key):
+    chosen_hash = hashes.SHA256()
+    hasher = hashes.Hash(chosen_hash, default_backend())
+    hasher.update(b"data & ")
+    hasher.update(b"more data")
+    digest = hasher.finalize()
+    Public_ec_key.verify(
+        signature,
+        digest,
+        ec.ECDSA(utils.Prehashed())
 
+    )
 if __name__=="__main__":
 
     Dir,pubk,prik,sub = parse_args()
+
+    publickey = open(pubk,'rb')
+    pub = publickey.read()
+    keysig = open('keyfile.sig','rb')
+    signed = keysig.read()
+
+    cert = x509.load_pem_x509_certificate(pub,default_backend())
+    verify(signed, cert.public_key())
+    print(signed)
+
+
+
+
+    
